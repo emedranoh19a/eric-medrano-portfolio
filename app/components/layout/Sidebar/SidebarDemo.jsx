701521e885840f136997ded17a89d3c60ad05d73
@WSS-Eric <e-medrano@ws-sol.co.jp>
2024-12-31 00:34:28 +0900

    add fancy corner to sidebar

--- app/components/layout/Sidebar/SidebarDemo.jsx
+++ app/components/layout/Sidebar/SidebarDemo.jsx
@@ -2,6 +2,7 @@
 
 import { sidebarLinks } from "@/app/data";
 import { cn } from "@/app/utils/utils";
+import clsx from "clsx";
 import { useState } from "react";
 import { Sidebar, SidebarBody } from "./Sidebar";
 import SidebarLink from "./SidebarLink";
@@ -25,9 +26,8 @@ export function SidebarDemo({ children }) {
       <Sidebar open={open} setOpen={setOpen}>
         {/* Display related styles will conflict with "hidden" */}
         <SidebarBody className="gap-10 relative self-stretch h-full w-full">
-          {/* TODO: Insert here a fancy corner */}
-
           <div className="top-0 left-0 p-0 fixed w-[inherit]">
+            <InvertedCorner />
             <div className="flex flex-col overflow-x-hidden px-2 mt-2 h-fit">
               {open ? <SidebarLogo /> : <SidebarLogoIcon />}
               <div
@@ -38,31 +38,33 @@ export function SidebarDemo({ children }) {
                 ))}
               </div>
             </div>
-
-            {/* <div>
-              <SidebarLink
-                link={{
-                  label: "Eric Medrano",
-                  href: "#",
-                  // TODO Add image of me.
-                  // icon: (
-                  //   <Image
-                  //     src="https://assets.aceternity.com/manu.png"
-                  //     className="h-7 w-7 flex-shrink-0 rounded-full"
-                  //     width={50}
-                  //     height={50}
-                  //     alt="Avatar"
-                  //   />
-                  // ),
-                  icon: "", //This is essentially, my avatar, .
-                }}
-              />
-            </div> */}
           </div>
         </SidebarBody>
       </Sidebar>
       <div className="h-fit overflow-y-scroll flex-1 mx-auto w-full">
         {children}
+      </div>
+    </div>
+  );
+}
+
+function InvertedCorner() {
+  const positioningStyles = clsx(
+    " w-full h-40 absolute top-0 right-0 translate-x-full -translate-y-full"
+  );
+  //Step 2: Create a pseudoelement
+  const pseudoStyles = clsx(
+    "absolute bg-transparent bottom-0 translate-y-full h-60 w-20 rounded-tl-[4rem]"
+  );
+  //Step 3: Add a shadow to the pseudoelement
+  //We want to fill the space between the div and the pseudoelement. the best way is to use a box shadow.
+  return (
+    <div className={positioningStyles}>
+      <div className="relative w-40 h-full rounded-[6rem] rounded-bl-none bg-[#f5f5f5]">
+        <div
+          className={pseudoStyles}
+          style={{ boxShadow: "0 -6rem 0 0 #f5f5f5" }}
+        ></div>
       </div>
     </div>
   );
