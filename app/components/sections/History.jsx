76bb10b506485f046b0443170a6ad654af14f30b
@WSS-Eric <e-medrano@ws-sol.co.jp>
2025-01-28 18:01:37 +0900

    fix netlify errors

--- app/components/sections/History.jsx
+++ app/components/sections/History.jsx
@@ -12,18 +12,18 @@ export default function History() {
   );
 }
 
-function Historycontent() {
-  return (
-    <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-      <BoxReveal className="block mx-auto">
-        <Heading text="History" className="text-center block" />
-      </BoxReveal>
-      <BoxReveal className="mx-auto">
-        <Text className="text-center block">
-          Welcome to my timeline! Explore the moments that have shaped my
-          journey.
-        </Text>
-      </BoxReveal>
-    </div>
-  );
-}
+// function Historycontent() {
+//   return (
+//     <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
+//       <BoxReveal className="block mx-auto">
+//         <Heading text="History" className="text-center block" />
+//       </BoxReveal>
+//       <BoxReveal className="mx-auto">
+//         <Text className="text-center block">
+//           Welcome to my timeline! Explore the moments that have shaped my
+//           journey.
+//         </Text>
+//       </BoxReveal>
+//     </div>
+//   );
+// }
