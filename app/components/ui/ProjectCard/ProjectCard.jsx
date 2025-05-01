03d5d9290a2d4ff04774e753ac094751a8cc86b4
@WSS-Eric <e-medrano@ws-sol.co.jp>
2025-05-01 18:39:13 +0900

    save changes and reupload

--- app/components/ui/ProjectCard/ProjectCard.jsx
+++ app/components/ui/ProjectCard/ProjectCard.jsx
@@ -3,18 +3,6 @@
 import ProjectCardDesktop from "./ProjectCardDesktop";
 import ProjectCardMobile from "./ProjectCardMobile";
 
-const randomDescriptions = [
-  "This one is still finding itself.",
-  "The code is still baking.",
-  "One more coffee brew to release.☕️",
-  "Still in the markup limbo.",
-  "The paint is still wet. 🎨",
-  "Letting this one grow. 🌱",
-  "Still arguing with me over which font to use.",
-  "Under construction by an overcaffeinated squirrel (me).",
-  "Refuses to build unless I say 'please'. 🙃",
-];
-
 export default function ProjectCard({
   className,
   index,
