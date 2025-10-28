a77a12e7a374021306b336213fd0d88b3c19091c
@WSS-Eric <e-medrano@ws-sol.co.jp>
2025-10-28 15:29:07 +0900

    last changes of manage-landing-page before migrating to windows

--- app/projects/intermediate/manage-landing-page/components/ui/Carousel/Slide.jsx
+++ app/projects/intermediate/manage-landing-page/components/ui/Carousel/Slide.jsx
@@ -29,19 +29,19 @@ export default function Slide({ slide, index, current, handleSlideClick }) {
     };
   }, []);
 
-  const handleMouseMove = (event) => {
-    const el = slideRef.current;
-    if (!el) return;
+  // const handleMouseMove = (event) => {
+  //   const el = slideRef.current;
+  //   if (!el) return;
 
-    const r = el.getBoundingClientRect();
-    xRef.current = event.clientX - (r.left + Math.floor(r.width / 2));
-    yRef.current = event.clientY - (r.top + Math.floor(r.height / 2));
-  };
+  //   const r = el.getBoundingClientRect();
+  //   xRef.current = event.clientX - (r.left + Math.floor(r.width / 2));
+  //   yRef.current = event.clientY - (r.top + Math.floor(r.height / 2));
+  // };
 
-  const handleMouseLeave = () => {
-    xRef.current = 0;
-    yRef.current = 0;
-  };
+  // const handleMouseLeave = () => {
+  //   xRef.current = 0;
+  //   yRef.current = 0;
+  // };
 
   const imageLoaded = (event) => {
     event.currentTarget.style.opacity = "1";
@@ -55,19 +55,22 @@ export default function Slide({ slide, index, current, handleSlideClick }) {
         ref={slideRef}
         className="flex flex-1 flex-col items-center justify-center relative text-center text-white opacity-100 transition-all duration-300 ease-in-out w-[70vmin] h-[70vmin] mx-[4vmin] z-10 "
         onClick={() => handleSlideClick(index)}
-        onMouseMove={handleMouseMove}
-        onMouseLeave={handleMouseLeave}
-        style={{
-          transform:
-            current !== index
-              ? "scale(0.98) rotateX(8deg)"
-              : "scale(1) rotateX(0deg)",
-          transition: "transform 0.5s cubic-bezier(0.4, 0, 0.2, 1)",
-          transformOrigin: "bottom",
-        }}
+        // onMouseMove={handleMouseMove}
+        // onMouseLeave={handleMouseLeave}
+        style={
+          {
+            // transform:
+            //   current !== index
+            //     ? "scale(0.98) rotateX(8deg)"
+            //     : "scale(1) rotateX(0deg)",
+            // transition: "transform 0.5s cubic-bezier(0.4, 0, 0.2, 1)",
+            // transformOrigin: "bottom",
+          }
+        }
       >
+        {/* background */}
         <div
-          className="absolute top-0 left-0 w-full h-full bg-[#1D1F2F] rounded-[1%] overflow-hidden transition-all duration-150 ease-out"
+          className="absolute top-0 left-0 w-full h-full bg-red-500 rounded-[1%] overflow-hidden transition-all duration-150 ease-out"
           style={{
             transform:
               current === index
@@ -75,7 +78,7 @@ export default function Slide({ slide, index, current, handleSlideClick }) {
                 : "none",
           }}
         >
-          <img
+          {/* <img
             className="absolute inset-0 w-[120%] h-[120%] object-cover opacity-100 transition-opacity duration-600 ease-in-out"
             style={{
               opacity: current === index ? 1 : 0.5,
@@ -88,24 +91,29 @@ export default function Slide({ slide, index, current, handleSlideClick }) {
           />
           {current === index && (
             <div className="absolute inset-0 bg-black/30 transition-all duration-1000" />
-          )}
+          )} */}
         </div>
 
-        <article
-          className={`relative p-[4vmin] transition-opacity duration-1000 ease-in-out ${
-            current === index ? "opacity-100 visible" : "opacity-0 invisible"
-          }`}
-        >
-          <h2 className="text-lg md:text-2xl lg:text-4xl font-semibold  relative">
-            {title}
-          </h2>
-          <div className="flex justify-center">
-            <button className="mt-6  px-4 py-2 w-fit mx-auto sm:text-sm text-black bg-white h-12 border border-transparent text-xs flex justify-center items-center rounded-2xl hover:shadow-lg transition duration-200 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-              {button}
-            </button>
-          </div>
-        </article>
+        <TestimonialCard />
       </li>
     </div>
+  );
+}
+
+function TestimonialCard() {
+  return (
+    <article
+      className={`relative p-[4vmin] transition-opacity duration-1000 ease-in-out`}
+    >
+      <div className="w-18 aspect-square relative rounded-full overflow-hidden bg-lime-500">
+        I
+      </div>
+      <h3 className="preset-4-bold text-blue-950">Ali Bravo</h3>
+      <p className="preset-5-regular text-blue-950/50">
+        “We have been able to cancel so many other subscriptions since using
+        Manage. There is no more cross-channel confusion and everyone is much
+        more focused.”
+      </p>
+    </article>
   );
 }
