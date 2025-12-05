5951d99a6d2b33e226159c548f98a674b39b5f12
メドラノ エリック <e-medrano@wssol.onmicrosoft.com>
2025-12-05 14:22:31 +0900

    fix useCountdown import error for netlify

--- app/projects/beginner/officelite-coming-soon-site/components/ui/Counter.jsx
+++ app/projects/beginner/officelite-coming-soon-site/components/ui/Counter.jsx
@@ -1,6 +1,6 @@
 "use client";
 import clsx from "clsx";
-import { useCountdown } from "../../hooks/usecountdown";
+import { useCountdown } from "../../hooks/useCountdown";
 
 const months = [
   "JAN",
